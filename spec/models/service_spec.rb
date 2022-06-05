7803c30b10209ea5b337727e7194a7b5e6689b02
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-06-04 23:46:13 -0500

    chore: testing models, rspec setup, models validations

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:price)}
+     #testing relationships between models
+     it { should belong_to(:area) }
+     it { should validate_presence_of(:name) }
+     it 'validate name' do 
+         area = Area.create(name: 'aasdsf', description: 'aasdsf')
+         subject.area = area
+         should allow_value(Faker::Name.first_name).for(:name) 
+     end
+     #testing price
+     it { should validate_presence_of(:price)}
+     it { should validate_numericality_of(:price)}
+     it { should allow_value(Faker::Number.decimal).for(:price) }
+     it { should_not allow_value(Faker::String.random).for(:price)}
 end
