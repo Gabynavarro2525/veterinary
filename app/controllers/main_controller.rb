c8da4db255f3000131dda5d0f51fffd62bb34b38
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-06-25 00:28:56 -0500

    chore: contollers start the controllers of the first model

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,6 @@
+class MainController < ApplicationController
+    def principalPage
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'products/index', to: 'products#index'
+  get 'products/new', to: 'products#new'
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "main#principalPage"
 end
