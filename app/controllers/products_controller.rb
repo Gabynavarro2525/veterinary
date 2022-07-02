7d00c5977b9fa839e34cf016f896b36795c91e04
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-07-01 23:07:42 -0500

    chore: contollers new views and modified files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,24 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all
+   # render  json: @products
   end
-
   def new
-
+    @products = Product.new
+  end
+  def create
+     @products = Product.create product_params
+     redirect_to products_path, notice: 'Product was successfully created.'
+  end
+  def show 
+    @products = Product.find(params[:name])
+  end
+  def edit
   end
 
-end
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :selling_price, :purchase_price)
+  end
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index', to: 'products#index'
-  get 'products/new', to: 'products#new'
+  resources :products, only: [:index, :new, :create, :show]
   # Defines the root path route ("/")
    root "main#principalPage"
 end
