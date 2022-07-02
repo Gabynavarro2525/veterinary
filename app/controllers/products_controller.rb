a4c8a3e56856a4881bf1ea5997bbaad285058ac1
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-07-02 08:32:39 -0500

    chore: contollers last views created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,37 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
    # render  json: @products
   end
   def new
-    @products = Product.new
+    @product = Product.new
   end
   def create
-     @products = Product.create product_params
-     redirect_to products_path, notice: 'Product was successfully created.'
+    @product = Product.create product_params
+    redirect_to product_path(@product), notice: 'Product was successfully created.'
   end
   def show 
-    @products = Product.find(params[:name])
   end
   def edit
+  end
+  def update
+    if @product.update product_params
+      redirect_to product_path(@product), notice: 'Product was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+  end
+  def destroy
+    @product.destroy
+    redirect_to products_path, status: :see_other, notice: 'Product deleted successfully'
   end
 
   private
   def product_params
     params.require(:product).permit(:name, :description, :selling_price, :purchase_price)
   end
+  def set_product
+    @product = Product.find params[:id]
   end
+end
 
