88e02340f5617b82621c711134169f71f450fdc3
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-07-29 20:54:10 -0500

    chore: controller show and create products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create product_params
-    redirect_to product_path(@product), notice: 'Product was successfully created.'
+    redirect_to products_path(@product), notice: 'Product was successfully created.'
   end
 
   def show 
@@ -22,7 +22,7 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update product_params
-      redirect_to products_path(@product), notice: 'Product was successfully updated.'
+      redirect_to products_path, notice: 'Product was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,7 +35,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :selling_price, :purchase_price)
+    params.require(:product).permit(:area_id, :name, :description, :selling_price, :purchase_price)
   end
   def set_product
     @product = Product.find params[:id]
