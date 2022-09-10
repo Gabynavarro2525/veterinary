caaab191d9323113f551bc86f65c88dc12ba593c
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-09-10 11:31:12 -0500

    chore: pdf_files test fixed

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,31 +1,23 @@
-#preguntar si esta bien
 require 'rails_helper'
+
 RSpec.describe "Products", type: :request do
   describe "GET /products" do
      let!(:products) { FactoryBot.create_list(:product, 4) }
+
      it "contains the product names" do
         get "/products"
-        #preguntar por esto
-        expect(response).to include products.name
-        expect(response).to include products.classification
-        expect(response).to include products.purchase_price
-        expect(response).to include products.selling_price
+        expect(response.status).to eq 200
+        #for product in products do
+         # expect(response.body).to include(product.name)
+        #end
+        products.each do |product|
+          expect(response.body).to include(product.name)
+        end
      end
-  end
+      it "contains the product names" do
+       get "/products", params:{format: :pdf}
+       expect(response.status).to eq 200
+       expect(response.content_type).to eq('application/pdf')
+      end
+   end
 end
-
-# Gemfile
-#gem 'capybara'
-# spec/features/products_spec.rb
-#require 'rails_helper'
-#RSpec.feature "Products" do
-#  let!(:products) { FactoryBot.create_list(:product, 4) }
-
-#  scenario "when a user views a product" do
-#    visit '/'
-#    click_link 'Products'
-#    click_link products.first.name
-#    expect(page).to have_content products.first.name
-#    expect(page).to have_content products.first.description
-#  end
-#end
