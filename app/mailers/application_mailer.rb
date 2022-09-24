836d76aada337911326296cbd2e76bd301357695
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-09-24 10:55:02 -0500

    chore: excel_files aplication_mailer fixed

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  def send_mail_files
-    mail(to: "gabynavarro2525@gmail.com", from: "djohnston@ucol.mx", subject: "Welcome to Veterinary page", message: "Body of the message")
-  end
+  default from: "from@example.com"
+  layout "mailer"
 end
