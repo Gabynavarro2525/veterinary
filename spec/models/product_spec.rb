90b9a4485dec7b43c5f167f15ce842936bc2d00e
Gabriela Johnston <gabynavarro2525@gmail.com>
2022-10-15 10:19:57 -0500

    chore: mailer test mailer

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,36 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Product, type: :model do
-  it 'testing relationships between models'do
-    should belong_to(:area) 
-    should validate_presence_of(:name) 
+  it "Testing mailer" do
+    expect(mail.subject).to eq("Signup")
+    expect(mail.to).to eq(["gabynavarro2525@gmail.com"])
+    expect(mail.from).to eq(["djohnston@ucol.mx"])
   end
-
-  it 'validate name' do
-    area = create(:area)
-    subject.area = area
-    should allow_value(Faker::Name.first_name).for(:name)
-    should_not allow_value(Faker::Number.number).for(:name)
+  it "body of the mail" do
+    expect(mail.body.encoded).to match("Download report in pdf format")
   end
- 
-  it 'testing classification' do 
-    should validate_presence_of(:classification)
-    should allow_value(Faker::Lorem.word).for(:classification)
-    should_not allow_value(Faker::Number.number).for(:classification)
-    
-  end
-  it 'testing selling_price' do
-    should validate_presence_of(:selling_price)
-    should validate_numericality_of(:selling_price)
-    should allow_value(Faker::Number.decimal).for(:selling_price)
-    should_not allow_value(Faker::String.random).for(:selling_price)
-   
-  end
-  it 'testing purchase_price' do
-    should validate_presence_of(:purchase_price)
-    should validate_numericality_of(:purchase_price)
-    should allow_value(Faker::Number.decimal).for(:purchase_price)
-    should_not allow_value(Faker::String.random).for(:purchase_price)
-  end
-  
 end
