29234499a269bfb6b7f06ebec2b9c87713c8e4a8
Jorge Precich <jorge.precich@finalsite.com>
2022-11-05 11:37:39 -0600

    fix: some inconsistencies

--- Delete_later/array_exersice.js
+++ Delete_later/array_exersice.js
@@ -1,21 +1,20 @@
-function sonConsecutivos(newarray )
-{
-        if(newarray.length < 2) return -1;
-        let i = 0;
-        while(i < newarray.length - 1) {
-            if(Math.abs(newarray[i] - newarray[++i]) !== 1) return -1;
-            
-            console .log ("0 vueltas");
-            return 0;
-        }
-for ( var x; x <newarray.length; x++){
-  l= 1
-  if (newarray[x]>newarray[l]){
-    x = x++
-    l = l++
-    console.log(i, "vueltas");
+function sonConsecutivos(newarray) {
+  if(newarray.length < 1) return -1;
+
+  let i = 0;
+  while(i < newarray.length - 1) {
+    if (Math.abs(newarray[i] - newarray[++i]) !== 1) return -1;
+
+    console.log("0 vueltas");
+    return 0;
+  }
+
+  for (let x = 0; x < newarray.length; x++) {
+    if (newarray[x] > newarray[x++]){
+      x = x++
+      console.log(x, "vueltas");
+    }
+  }
 }
-}
-}
-let  arrayvalor = [1,2,3,4];
-let finalarray = sonConsecutiv
+let  arrayvalor = [1,2];
+sonConsecutivos(arrayvalor)
